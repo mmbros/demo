5d24bb65e5fa2d044fec417bc31dcd19e360aa24
mmbros <server.mmbros@yandex.com>
2017-01-18 23:26:33 +0100

    added some sources

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ References:
 - [Go Concurrency Patterns: Context](https://blog.golang.org/context)
 - [Go Concurrency Patterns: Pipelines and cancellation](https://blog.golang.org/pipelines)
 
+- [Beautiful Go patterns for concurrent access to shared resources and coordinating responses](http://dieter.plaetinck.be/post/beautiful_go_patterns_for_concurrent_access_to_shared_resources_and_coordinating_responses/)
+- [Go by Example: Worker Pools](https://gobyexample.com/worker-pools)
+
 https://gist.github.com/tmiller/5550127
 A very simple example of using a map of channels for pub/sub in go.
 */
