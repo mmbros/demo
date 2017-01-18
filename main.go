b1d6b113918605b8f511757e01c061364667732d
mmbros <server.mmbros@yandex.com>
2017-01-18 22:01:52 +0100

    wip

--- main.go
+++ main.go
@@ -18,40 +18,10 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"strconv"
-	"sync"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
-
-// Result cointains the informations returned by a stock price scraper.
-type Result struct {
-
-	// name of the scraper that get the results
-	ScraperName string
-	// url of the html page
-	URL string
-	// stock identifier
-	StockId string
-	// timestamps
-	TimeStart, TimeEnd time.Time
-
-	StockPrice string
-	StockDate  string
-	Err        error
-}
-
-type ParseDocFunc func(doc *goquery.Document) (price string, date string, err error)
-
-type Scraper struct {
-	Name               string
-	Disabled           bool
-	ConcurrentRequests int
-	ParseDoc           ParseDocFunc
-	ch                 chan *ScraperRequest
-}
-
-type Scrapers map[string]*Scraper
 
 type StockPriceSource struct {
 	ScraperName string
@@ -69,52 +39,6 @@ type Stock struct {
 
 type Stocks map[string]*Stock
 
-func (scraper *Scraper) GetStockPrice(ctx context.Context, stockId, url string) *Result {
-	if scraper == nil {
-		panic("GetStockPrice: scraper is nil")
-	}
-
-	result := &Result{
-		ScraperName: scraper.Name,
-		URL:         url,
-		StockId:     stockId,
-		TimeStart:   time.Now(),
-	}
-	// use defer to set timeEnd
-	defer func() { result.TimeEnd = time.Now() }()
-
-	// error in case of disabled scraper
-	if scraper.Disabled {
-		result.Err = errors.New("Disabled scraper")
-		return result
-	}
-
-	// get the response
-	resp, err := GetUrl(ctx, url)
-	if err != nil {
-		result.Err = err
-		return result
-	}
-
-	// create goquery document
-	doc, err := goquery.NewDocumentFromResponse(resp)
-	if err != nil {
-		result.Err = err
-		return result
-	}
-	// parse the response
-	price, date, err := scraper.ParseDoc(doc)
-	if err != nil {
-		result.Err = err
-		return result
-	}
-
-	result.StockPrice = price
-	result.StockDate = date
-
-	return result
-}
-
 type ScraperRequest struct {
 	stockId     string
 	scraperName string
@@ -123,97 +47,9 @@ type ScraperRequest struct {
 	cancel      context.CancelFunc
 }
 
-func (scraper *Scraper) dowork(requests <-chan *ScraperRequest, results chan<- *Result) {
-	for req := range requests {
-		results <- scraper.GetStockPrice(req.ctx, req.stockId, req.url)
-	}
-}
-
-func (scraper *Scraper) WorkRequests(requests <-chan *ScraperRequest) {
-
-	if scraper == nil {
-		panic("WorkRequest: scraper is nil")
-	}
-
-	c := make(chan *Result)
-
-	// start a fixed number of worker
-	numWorkers := scraper.ConcurrentRequests
-	if numWorkers < 1 {
-		numWorkers = 1
-	}
-	var wg sync.WaitGroup
-	wg.Add(numWorkers)
-
-	for j := 0; j < numWorkers; j++ {
-		go func() {
-			scraper.dowork(requests, c)
-			wg.Done()
-		}()
-	}
-
-	go func() {
-		wg.Wait()
-		close(c)
-	}()
-}
-
-func GetUrl(ctx context.Context, url string) (*http.Response, error) {
-
-	type result struct {
-		resp *http.Response
-		err  error
-	}
-
-	select {
-	case <-ctx.Done():
-		return nil, ctx.Err()
-	default:
-		// make the request
-		tr := &http.Transport{}
-		client := &http.Client{Transport: tr}
-
-		req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			return nil, err
-		}
-
-		c := make(chan result, 1)
-
-		go func() {
-			resp, err := client.Do(req)
-			c <- result{resp: resp, err: err}
-		}()
-
-		select {
-		case <-ctx.Done():
-			tr.CancelRequest(req)
-			<-c // Wait for client.Do
-			return nil, ctx.Err()
-		case r := <-c:
-			return r.resp, r.err
-		}
-	}
-}
-
-func borsaitaliana(doc *goquery.Document) (price string, date string, err error) {
-
-	doc.Find("div.l-box > div.l-box > span > strong").Each(func(i int, s *goquery.Selection) {
-		switch i {
-		case 0:
-			price = s.Text()
-		case 3:
-			date = s.Text()
-		}
-	})
-	if price == "" {
-		err = errors.New("Price not found")
-	}
-	return
-}
 func testsource(doc *goquery.Document) (price string, date string, err error) {
 
-	doc.Find("ul > li").Each(func(i int, s *goquery.Selection) {
+	doc.Find("ul > li > b").Each(func(i int, s *goquery.Selection) {
 		switch i {
 		case 0:
 			price = s.Text()
@@ -248,7 +84,11 @@ func initTestStockServer() *httptest.Server {
 }
 
 func getScraperName(n int) string {
-	name := fmt.Sprintf("scraper%d", n)
+	name := fmt.Sprintf("scraper_%d", n)
+	return name
+}
+func getStockName(n int) string {
+	name := fmt.Sprintf("stock_%d", n)
 	return name
 }
 
@@ -258,9 +98,7 @@ func initScrapers(numScrapers int) Scrapers {
 		name := getScraperName(j)
 		scrapers[name] = &Scraper{
 			Name:     name,
-			Disabled: false,
 			ParseDoc: testsource,
-			ch:       make(chan *ScraperRequest),
 		}
 	}
 	return scrapers
@@ -272,7 +110,7 @@ func initTestStocks(numStocks, numScrapers int, url string) Stocks {
 	newSpi := func(numScraper, numStock int) StockPriceSource {
 		var spi StockPriceSource
 		spi.ScraperName = getScraperName(numScraper)
-		spi.URL = fmt.Sprintf("%s/%s/stock%d", url, spi.ScraperName, numStock)
+		spi.URL = fmt.Sprintf("%s/%s/%s", url, spi.ScraperName, getStockName(numStock))
 		return spi
 	}
 
@@ -394,7 +232,30 @@ func channelizeRequest(ctx context.Context, stocks Stocks) <-chan *ScraperReques
 //}()
 
 //}
+// First runs query on replicas and returns the first result.
+func (s *Stock) GetStockPrice2(ctx context.Context, scrapers Scrapers) *Result {
 
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	c := make(chan *Result, len(s.Sources))
+
+	search := func(spi StockPriceSource) {
+		scr := scrapers[spi.ScraperName]
+		c <- scr.GetStockPrice(ctx, s.ID, spi.URL)
+	}
+
+	for _, replica := range s.Sources {
+		go search(replica)
+	}
+
+	select {
+	case <-ctx.Done():
+		return &Result{Err: ctx.Err()}
+	case r := <-c:
+		return r
+	}
+}
 func main() {
 	t1 := time.Now()
 
@@ -414,7 +275,7 @@ func main() {
 	//scraper := scrapers[spi.ScraperName]
 	//res := scraper.GetStockPrice(ctx, stock0.ID, spi.URL)
 
-	res := stocks["id0"].GetStockPrice(ctx, scrapers)
+	res := stocks["id9"].GetStockPrice(ctx, scrapers)
 
 	fmt.Printf("res = %+v\n", res)
 	fmt.Printf("Elapsed = %+v\n", res.TimeEnd.Sub(res.TimeStart))
